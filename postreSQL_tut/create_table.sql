f540d0f9fb9666aa06a7d452ab1a82056609a826
Mathafeni Philasande <labale1996@gmail.com>
2026-07-03 21:36:28 +0200

    done

--- postreSQL_tut/create_table.sql
+++ postreSQL_tut/create_table.sql
@@ -2,13 +2,13 @@
 --We will create a table
 --we will create cars table
 
-CREATE TABLE cars(
-id int PRIMARY KEY,
+CREATE TABLE cars( --statement to create table 
+id int PRIMARY KEY, --set the primary of the table 
 car_name VARCHAR(50), --create the  column name and the data type
 price NUMERIC(10,2),
 model VARCHAR(50),
 year INT,
 brand VARCHAR(50),
-created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, --add  auto date and  time 
 updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+);                   
